1dbec2f8bff209195b5e0f4fcb29e2b35af5a831
gupi9163-lab <gupi9163-lab@users.noreply.github.com>
2026-02-28 11:58:00 +0000

    Refactor to static PWA site

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono'
-import { serveStatic } from 'hono/cloudflare-workers'
-
-const app = new Hono()
-
-// Serve static files from public directory
-app.use('/*', serveStatic({ root: './public' }))
-
-export default app
